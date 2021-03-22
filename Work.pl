14c52beec9f1b4fea2712d0fcf3d5a57ae11e9be
Rozz <rubicon868@mail.ru>
2021-03-22 11:36:28 +0300

    lab 3 , num 6

--- Work.pl
+++ Work.pl
@@ -13,4 +13,8 @@ factorial(N,X,X1):-N1 is N-1,X2 is X1*N,factorial(N1,X,X2).
 
 fib(1,1):-!.
 fib(2,1):-!.
-fib(N,X):-N1 is N-1,N2 is N-2,fib(N1,X1),fib(N2,X2),X is X1+X2 .
+fib(N,X):-N1 is N-1,N2 is N-2,fib(N1,X1),fib(N2,X2),X is X1+X2.
+
+fibonach(N,N,X,_,X):-!.
+fibonach(N,N_nac,Xt,Cur_x,X):-N_nac1 is N_nac+1,Xt1 is Xt + Cur_x,fibonach(N,N_nac1,Xt1,Xt,X).
+fib1(N,X):-fibonach(N,1,1,0,X).
