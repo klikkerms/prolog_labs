8022c2cdd6f04c5a8d0bea8564a1c01e9d6aa991
Rozz <rubicon868@mail.ru>
2021-03-22 12:07:55 +0300

    lab 3 , num 7

--- Work.pl
+++ Work.pl
@@ -18,3 +18,6 @@ fib(N,X):-N1 is N-1,N2 is N-2,fib(N1,X1),fib(N2,X2),X is X1+X2.
 fibonach(N,N,X,_,X):-!.
 fibonach(N,N_nac,Xt,Cur_x,X):-N_nac1 is N_nac+1,Xt1 is Xt + Cur_x,fibonach(N,N_nac1,Xt1,Xt,X).
 fib1(N,X):-fibonach(N,1,1,0,X).
+
+sum(X,S):- 0 is X div 10,S=X,!.
+sum(X,S):-X_1 is X div 10,sum(X_1,S_1),S is S_1+X mod 10.
