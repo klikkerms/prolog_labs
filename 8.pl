3f91b8d984246383abe107224fa9323bfb6596f7
Rozz <rubicon868@mail.ru>
2021-06-03 20:49:09 +0300

    lab 8 , num 1.1

---
+++ 8.pl
@@ -0,0 +1,35 @@
+write_str([]):-!.
+write_str([H|Tail]):-put(H),write_str(Tail).
+
+read_str(A,N,Flag):-get0(X),r_str(X,A,[],N,0,Flag).
+r_str(-1,A,A,N,N,1):-!.
+r_str(10,A,A,N,N,0):-!.
+r_str(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1,Flag).
+
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+
+read_list_str(List):-read_str(A,_,Flag),read_list_str([A],List,Flag).
+read_list_str(List,List,1):-!.
+read_list_str(Cur_list,List,0):-
+read_str(A,_,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
+
+get_word_space(Str,Word,Str2):-get_word_space(Str,[],Word,Str2).
+get_word_space([],Word,Word,[]).
+get_word_space([H|T],Word,Word,T):-H=32,!.
+get_word_space([H|T],Word,Word1,Str2):-append(Word,[H],Word2),get_word_space(T,Word2,Word1,Str2).
+
+%1.1
+prlongest:- see('C:/Users/Rozz/Desktop/Roma/1.txt'), read_list_str(_, LengthList),
+seen, max(LengthList, Max), write(Max).
+
+read_list_str(List, LengthList):-read_str(A,N,Flag),read_list_str([A],List,[N],LengthList,Flag).
+read_list_str(List,List,LengthList, LengthList,1):-!.
+read_list_str(Cur_list,List,CurLengthList,LengthList,0):-
+read_str(A,N,Flag),append(Cur_list,[A],C_l),append(CurLengthList, [N], NewLengthList),read_list_str(C_l,List,NewLengthList,LengthList,Flag).
+
+max(List, MaxEl):- max(List, 0, MaxEl).
+max([],CurMax, CurMax):- !.
+max([H|T], CurMax, X):- H > CurMax, NewMax is H, max(T,NewMax,X), !.
+max([_|T], CurMax, X):- max(T, CurMax, X).
--- Work.pl
+++ Work.pl
@@ -1,25 +1,25 @@
-max(X,Y,Z):-X>Y,Z is X,!.
+max(X,Y,Z):-X>Y,Z is X,!.%1
 max(_,Y,Y):-!.
 
 
-max(X,Y,Z,U):-max(X,Y,R),max(R,Z,U).
+max(X,Y,Z,U):-max(X,Y,R),max(R,Z,U).%2
 
-fact(1,1):-!.
+fact(1,1):-!.%3
 fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
 
-fact(N,X):-factorial(N,X,1).
+fact(N,X):-factorial(N,X,1).%4
 factorial(1,X,X):-!.
 factorial(N,X,X1):-N1 is N-1,X2 is X1*N,factorial(N1,X,X2).
 
-fib(1,1):-!.
+fib(1,1):-!.%5
 fib(2,1):-!.
 fib(N,X):-N1 is N-1,N2 is N-2,fib(N1,X1),fib(N2,X2),X is X1+X2.
 
-fibonach(N,N,X,_,X):-!.
+fibonach(N,N,X,_,X):-!.%6
 fibonach(N,N_nac,Xt,Cur_x,X):-N_nac1 is N_nac+1,Xt1 is Xt + Cur_x,fibonach(N,N_nac1,Xt1,Xt,X).
 fib1(N,X):-fibonach(N,1,1,0,X).
 
-sum(X,S):- 0 is X div 10,S=X,!.
+sum(X,S):- 0 is X div 10,S=X,!.%7
 sum(X,S):-X_1 is X div 10,sum(X_1,S_1),S is S_1+X mod 10.
 
 
@@ -42,3 +42,23 @@ nod1(N1,N2,Nod):-max(N1,N2,Max),min(N1,N2,Min),M is Max-Min,((N1 =Max,nod1(M,N2,
 
 
 
+%12
+nod(N,N,N):-!.
+nod(N,M,X):-N>M, N1 is N-M,nod(N1,M,X),!.
+nod(N,M,X):-N<M, M1 is M-N,nod(N,M1,X),!.
+
+prost(1):-!.
+prost(2):-!.
+prost(N):-N>0,prost(N,2),!.
+prost(N,N):-!.
+prost(N,M):-0 is N mod M,!,fail.
+prost(N,M):-M1 is M+1,prost(N,M1),!.
+
+kol_del(N,X):-N1 is N div 2,kol_del(N,N1,X1),X is X1+1,!.
+kol_del(_,0,0):-!.
+kol_del(N,M,X):-M1 is M-1,C is N mod M,kol_del(N,M1,X1),(C=0,X is X1+1;C\=0,X is X1),!.
+
+%11
+d11(0,0):-!.
+d11(X,K):-X1 is X div 10,d11(X1,K1),0 is (X mod 10) mod 3,K is K1+(X mod 10).
+d11(X,K1):-X1 is X div 10,d11(X1,K1).
