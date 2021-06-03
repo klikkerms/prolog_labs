1e466a1e53ea054ca059f8c70309c34f6257c5ec
Rozz <rubicon868@mail.ru>
2021-06-03 20:51:41 +0300

    lab 8 m num 1.4

--- 8.pl
+++ 8.pl
@@ -73,4 +73,53 @@ read_list_str_A(_,_,1):-!.
 read_list_str_A(List,M,0):-
 read_str_symbol(A,Count,65,Flag), (Count > M -> write_str(A),nl; true),
 read_list_str_A(List,M,Flag).
+%1.4
+prfrequent:- see('C:/Users/Rozz/Desktop/Roma/1.txt'), read_list_str1(List), seen,
+freq_word(List,Freq_word), (Freq_word\=[] -> write("Word = "), write_str(Freq_word); false).
+
+read_list_str1(List):- read_str(A,_,Flag), append(A,[32],L),
+read_list_str1(L,List,Flag).
+read_list_str1(List,List,1):-!.
+read_list_str1(Cur_list,List,0):-
+read_str(A,_,Flag), append(Cur_list,A,C_l), append(C_l,[32],L),
+read_list_str1(L,List,Flag).
+
+in_list(_,El):-!.
+in_list([_|T],El):-in_list(T,El).
+
+
+get_word([],[]):- !.
+get_word([32|_],[]):- !.
+get_word([H|T1],[H|T2]):- H\=32, get_word(T1,T2),!.
+
+
+
+num_word([],_,I,I):-!.
+num_word([32|T],Word,I,C):- delete_space(T,List),
+num_word(List,Word,I,C), !.
+num_word(List,Word,I,C):- get_word(List,W), delete_fword(List,L),
+(W = Word -> I1 is I+1; I1 is I), num_word(L,Word,I1,C).
+
+append_list([],List2,List2).
+ 
+append_list([H|T1],List2,[H|T2]):- append_list(T1,List2,T2).
+
+delete_space([32|T],List):- delete_space(T,List),!.
+delete_space(List,List).
+
+
+delete_fword([],[]):-!.
+delete_fword([H|T1],List):- H\=32, delete_fword(T1,List),!.
+delete_fword([32|T],T):-!.
+
+
+
+freq_word(List,Freq_word):- delete_space(List,L), append_list(L,[32],Source_list),
+freq_word(Source_list,Source_list,[],0,Freq_word).
+freq_word(_,[],Word,_,Word):-!.
+freq_word(Source_list,Changing_list,Current_word,Num,Freq_word):-
+get_word(Changing_list,Cur), num_word(Source_list,Cur,0,I),
+delete_fword(Changing_list, List), delete_space(List,Changed_list),
+(I > Num -> freq_word(Source_list,Changed_list,Cur,I,Freq_word);
+freq_word(Source_list,Changed_list,Current_word,Num,Freq_word)).
 
