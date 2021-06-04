807e2f88f12958e11efae7ff8d5838813a44d333
Rozz <rubicon868@mail.ru>
2021-06-04 19:24:13 +0300

    lab 4 , num 1

---
+++ lab4.pl
@@ -0,0 +1,8 @@
+%1
+writeList([]):-!.
+writeList([H|T]):-write(H),write(" "),writeList(T).
+
+readList(N,L):-rList(N,0,[],L).
+rList(N,N,L,L):-!.
+rList(N,I,CurL,L):-I1 is I+1, read(X),append(CurL,[X],CurL1),rList(N,I1,CurL1,L).
+
