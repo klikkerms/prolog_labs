7339a244a920f0b57f0fca131f209d558ea70744
Rozz <rubicon868@mail.ru>
2021-06-04 19:27:42 +0300

    lab 4 , num 5

--- lab4.pl
+++ lab4.pl
@@ -51,3 +51,19 @@ read_list_el(N):-
     write("Number: "),list_el_numb(List,Elem,X),
     write(X),nl,false.
 
+%5
+readList(N,L):-rList(N,0,[],L).
+rList(N,N,L,L):-!.
+rList(N,I,CurL,L):-I1 is I+1, read(X),append(CurL,[X],CurL1),rList(N,I1,CurL1,L).
+
+list_el_numb([H|_],H,0):-!.
+list_el_numb([_|List],Elem,Numb):-list_el_numb(List,Elem,NewNumb),Numb is NewNumb+1.
+
+read_list_el(N):-
+    write("List: "),readList(N,List),nl,
+    write("Number: "),read(Numb),nl,
+    write("Element: "),list_el_numb(List,X,Numb),
+    write(X),nl,false.
+
+
+
