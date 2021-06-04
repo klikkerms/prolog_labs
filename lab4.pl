f5dcfe1d647757e30b11484b7d6fb9f4868298d2
Rozz <rubicon868@mail.ru>
2021-06-04 19:25:05 +0300

    lab 4 , num 2

--- lab4.pl
+++ lab4.pl
@@ -6,3 +6,17 @@ readList(N,L):-rList(N,0,[],L).
 rList(N,N,L,L):-!.
 rList(N,I,CurL,L):-I1 is I+1, read(X),append(CurL,[X],CurL1),rList(N,I1,CurL1,L).
 
+%2
+readList(N,L):-rList(N,0,[],L).
+rList(N,N,L,L):-!.
+rList(N,I,CurL,L):-I1 is I+1, read(X),append(CurL,[X],CurL1),rList(N,I1,CurL1,L).
+
+sum_list_down(List,Sum):-sListD(List,0,Sum).
+sListD([],Sum,Sum):-!.
+sListD([H|L],CurSum,Sum):-CurSum1 is CurSum + H, sListD(L,CurSum1,Sum).
+
+read_sum_list(N):-
+    readList(N,L),
+    sum_list_down(L,Sum),
+    nl, write("Sum = "),write(Sum),nl,fail.
+
