7175a28c5b2df5f76f9be2cd78fb306a10f29b13
Rozz <rubicon868@mail.ru>
2021-06-04 19:31:09 +0300

    lab 4 , num 11

--- lab4.pl
+++ lab4.pl
@@ -107,5 +107,12 @@ reverse(L,X):-rev(L,[],X).
 rev([],X,X):-!.
 rev([H|T],CurL,X):-rev(T,[H|CurL],X).
 
+%11
+ravenstvo([H],[H|_]):-!.
+ravenstvo([H|_],[H]):-!.
+ravenstvo([H|T],[H1|T1]):- H is H1 ,ravenstvo(T,T1).
+sravn([H|T],[H1|T1]):-ravenstvo([H|T],[H1|T1]).
+sravn([H|T],[_|T1]):-sravn([H|T],T1).
+
 
 
