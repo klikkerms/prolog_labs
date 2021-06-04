78d1e9e2f873761dce4a2b2b4d1cf54c1f030690
Rozz <rubicon868@mail.ru>
2021-06-04 19:55:33 +0300

    lab 9 , num 7

--- laba_9.pl
+++ laba_9.pl
@@ -186,6 +186,59 @@ in_free_pos7([H1,H2,H3,H4,H5,H6,H7],Sim):-(var(H1)->H1 is Sim),!;
 				  (var(H6)->H6 is Sim),!;
 				  (var(H7)->H7 is Sim).
 
+      %7
+number_7:-tell('C:/Users/Rozz/Desktop/лаба.txt'),not(number_7_),told.
+number_7_:-make_ar(9,Pos),sochet([H1,H2],2,Pos),in_list([97,98,99,100,101,102],Simbol1),
+put_pos9(Word,[H1,H2],[Simbol1]),
+
+in_list_exlude([97,98,99,100,101,102],Simbol1,NList),in_list_exlude(Pos,H1,NPos),in_list_exlude(NPos,H2,NNPos),
+
+sochet([H3, H4],2,NNPos),in_list(NList,Simbol2),put_pos9(Word,[H3,H4],[Simbol2]),
+
+in_list_exlude(NList,Simbol2,NList_),in_list_exlude(NNPos,H3,EPos),in_list_exlude(EPos,H4,Pos_),
+
+sochet(Pos1_,3,Pos_),in_list_exlude(NList_,Simbol3, LastSimbol),put_pos93(Word,Pos1_,[Simbol3]),
+		 in_list_exlude(LastSimbol,S1,LastSimbol1),
+		 in_free_pos9(Word,S1),
+		 in_list_exlude(LastSimbol1,S2,_),
+		 in_free_pos9(Word,S2),
+		 write_str(Word),nl,fail.
+
+put_pos9(Word,[Head1,Head2],[Sim]):-select_pos9(Word,Head1,Sim),select_pos9(Word,Head2,Sim).
+select_pos9(Word,Head,Sim):-(Head is 1->Word=[Sim,_,_,_,_,_,_,_,_],!);
+							(Head is 2->Word=[_,Sim,_,_,_,_,_,_,_],!);
+							(Head is 3->Word=[_,_,Sim,_,_,_,_,_,_],!);
+							(Head is 4->Word=[_,_,_,Sim,_,_,_,_,_],!);
+							(Head is 5->Word=[_,_,_,_,Sim,_,_,_,_],!);
+							(Head is 6->Word=[_,_,_,_,_,Sim,_,_,_],!);
+							(Head is 7->Word=[_,_,_,_,_,_,Sim,_,_],!);
+							(Head is 8->Word=[_,_,_,_,_,_,_,Sim,_],!);
+							(Head is 9->Word=[_,_,_,_,_,_,_,_,Sim]).
+
+put_pos93(Word,[Head1,Head2,Head3],[Sim]):-select_pos9(Word,Head1,Sim),select_pos9(Word,Head2,Sim), select_pos9(Word,Head3,Sim).
+select_pos9(Word,Head,Sim):-(Head is 1->Word=[Sim,_,_,_,_,_,_,_,_],!);
+							(Head is 2->Word=[_,Sim,_,_,_,_,_,_,_],!);
+							(Head is 3->Word=[_,_,Sim,_,_,_,_,_,_],!);
+							(Head is 4->Word=[_,_,_,Sim,_,_,_,_,_],!);
+							(Head is 5->Word=[_,_,_,_,Sim,_,_,_,_],!);
+							(Head is 6->Word=[_,_,_,_,_,Sim,_,_,_],!);
+							(Head is 7->Word=[_,_,_,_,_,_,Sim,_,_],!);
+							(Head is 8->Word=[_,_,_,_,_,_,_,Sim,_],!);
+							(Head is 9->Word=[_,_,_,_,_,_,_,_,Sim]).
+
+in_free_pos9([H1,H2,H3,H4,H5,H6,H7,H8,H9],Sim):-(var(H1)->H1 is Sim),!;
+				  (var(H2)->H2 is Sim),!;
+				  (var(H3)->H3 is Sim),!;
+				  (var(H4)->H4 is Sim),!;
+				  (var(H5)->H5 is Sim),!;
+				  (var(H6)->H6 is Sim),!;
+				  (var(H7)->H7 is Sim),!;
+				  (var(H8)->H8 is Sim),!;
+				  (var(H9)->H9 is Sim).
+
+
+
+
 
 
 
