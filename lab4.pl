8894d744d224f2ccbdc5a5ff537b51a11e2eee95
Rozz <rubicon868@mail.ru>
2021-06-04 19:28:42 +0300

    lab 4 , num 7

--- lab4.pl
+++ lab4.pl
@@ -73,3 +73,12 @@ min_list_up([H],H):-!.
 min_list_up([H|T],Min):-min_list_up(T,Min1),min(H,Min1,Min).
 
 
+%7
+min(X,Y,X):-X<Y,!.
+min(_,Y,Y):-!.
+
+min_list_down([H|T],Min):-min_l_d([H|T],H,Min).
+min_l_d([],Min,Min):-!.
+min_l_d([H|T],Tec,Min):-min(Tec,H,Tec1),min_l_d(T,Tec1,Min).
+
+
