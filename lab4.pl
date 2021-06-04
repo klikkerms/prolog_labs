aac276e0c97f5f36ee8b42d914c153d2d5b51d38
Rozz <rubicon868@mail.ru>
2021-06-04 19:34:16 +0300

    lab 4 , num 15

--- lab4.pl
+++ lab4.pl
@@ -129,3 +129,11 @@ check_is(X,[H|T]):- X =\= H,check_is(X,T).
 check([H|T]):-check_is(H,T),check(T).
 check([]):-!.
 
+%15
+delet_all([],_,[]):-!.
+delet_all([H|T],El,[H|T1]):-H=\=El,delet_all(T,El,T1),!.
+delet_all([_|T],El,T1):-delet_all(T,El,T1).
+
+make_this([],[]):-!.
+make_this([H|T],[H|T1]):-delet_all([H|T],H,New),make_this(New,T1).
+
