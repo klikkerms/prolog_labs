c51dbd10adde8287690a755f9d27803d5c9e09e5
Rozz <rubicon868@mail.ru>
2021-06-04 19:26:41 +0300

    lab 4 , num 3

--- lab4.pl
+++ lab4.pl
@@ -20,3 +20,16 @@ read_sum_list(N):-
     sum_list_down(L,Sum),
     nl, write("Sum = "),write(Sum),nl,fail.
 
+%3
+readList(N,L):-rList(N,0,[],L).
+rList(N,N,L,L):-!.
+rList(N,I,CurL,L):-I1 is I+1, read(X),append(CurL,[X],CurL1),rList(N,I1,CurL1,L).
+
+sum_list_up([],0):-!.
+sum_list_up([H|List],Sum):-sumListUp(List,SumNew),Sum is SumNew + H.
+
+read_sum_List(N):-
+    readList(N,L),
+    sum_list_up(L,Sum),
+    nl, write("Sum = "),write(Sum),nl,fail.
+
